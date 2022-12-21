1d63e80837795e1cc0d4ecec876bf31186df5e5f
Kabir Dhillon <kabirdhillon99@gmail.com>
2022-12-20 22:43:40 -0800

    Created APICaller Class and updated UI

---
+++ Instagram/APICaller.swift
@@ -0,0 +1,16 @@
+//
+//  APICaller.swift
+//  Instagram
+//
+//  Created by Kabir Dhillon on 6/13/22.
+//
+
+import Foundation
+
+// Create a APICaller class using URLSession (Bonus point: can you use URLSession.DataTaskPublisher Combine publisher to make network call instead)
+
+class APICaller: URLSession {
+    
+}
+
+
